dc2367a739f07540a68016042ca3131cdd1362b7
Oleg Gromov <mail@oleggromov.com>
2019-07-13 02:17:34 +0300

    Add copyFile

--- src/modules/fs-utils.js
+++ src/modules/fs-utils.js
@@ -4,16 +4,25 @@ const del = require('del')
 
 const cleanDir = folder => del.sync(path.join(folder, '*'))
 
-const writeFile = ({ filename, content }) => {
+const createDir = filename => {
   const dir = path.parse(filename).dir
-
   if (dir) {
     fs.mkdirSync(dir, { recursive: true })
   }
+}
+
+const writeFile = ({ filename, content }) => {
+  createDir(filename)
   fs.writeFileSync(filename, content)
+}
+
+const copyFile = ({ from, to }) => {
+  createDir(to)
+  fs.copyFileSync(from, to)
 }
 
 module.exports = {
   cleanDir,
-  writeFile
+  writeFile,
+  copyFile
 }
