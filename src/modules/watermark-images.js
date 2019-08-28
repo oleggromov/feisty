38b9e020f41c9032d4cf5558e8b0539ae5fe3be6
Oleg Gromov <mail@oleggromov.com>
2019-08-28 10:59:30 +0300

    Remove text size calculation

--- src/modules/watermark-images.js
+++ src/modules/watermark-images.js
@@ -1,19 +1,11 @@
 const gm = require('gm')
 
-const getImageSize = from => new Promise((resolve, reject) => {
-  gm(from).size((err, value) => {
-    if (err) return reject(err)
-    resolve(value)
-  })
-})
-
 const watermarkImage = ({ from, to, text }) =>
   new Promise(async (resolve, reject) => {
-    const size = await getImageSize(from)
     gm(from)
       .noProfile()
       .font('Arial')
-      .fontSize(Math.ceil(size.width / 50))
+      .fontSize(16)
       .gravity('SouthEast')
       .fill('white')
       .draw(`text 10,10 '${text}'`)
